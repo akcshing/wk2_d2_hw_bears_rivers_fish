27cb555f1e722b996c070f216ed4cefa42b16a92
Alexkcshing <alexkcshing@gmail.com>
2019-01-22 17:37:24 +0000

    add def init to bears class and add pathways to bears spec

--- bears.rb
+++ bears.rb
@@ -1,7 +1,14 @@
 class Bears
+  attr_accessor :name, :type, :food
 
-  def initialize
-    
+  def initialize(name, type, food)
+    @name = name
+    @type = type
+    @food = food
   end
+
+
+# A Bear should be able to take a Fish from the River and
+# add it to its collection of food
 
 end
--- specs/bears_spec.rb
+++ specs/bears_spec.rb
@@ -1,10 +1,22 @@
 require("minitest/autorun")
 require("minitest/rg")
+
 require_relative("../bears")
+require_relative("../rivers")
+require_relative("../fish")
 
 class BearsTest < MiniTest::Test
 
   def setup
+
+    @fish_1 = Fish.new("Salmon")
+    @fish_2 = Fish.new("Tuna")
+
+    @river_1 = Rivers.new ("Amazon", [@fish_1, @fish_2])
+
+    @starting_food = []
+
+    @bear_1 = Bear.new("Yogi", "Grizzly", [@starting_food])
 
   end
 
