74253c5bacf7e2048b58e7995644d10a5e0baa3e
Alexkcshing <alexkcshing@gmail.com>
2019-01-22 17:10:09 +0000

    add def setup and add req_rel fish class

--- specs/rivers_spec.rb
+++ specs/rivers_spec.rb
@@ -1,5 +1,17 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../rivers")
+require_relative("../fish")
 
 class RiversTest < MiniTest::Test
+
+  def setup
+    @fish_1 = Fish.new("Salmon")
+    @fish_2 = Fish.new("Tuna")
+
+    @river_1 = Rivers.new("Amazon", [@fish_1, @fish_2])
+
+  end
+
+
+end
